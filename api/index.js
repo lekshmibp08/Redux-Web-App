1a35457bcf153619bfb6eaa6944d68695293da23
Lekshmi <sreelekshmi310192@gmail.com>
2024-10-19 18:47:21 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
-import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import { error } from 'console';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');;        
+    }).catch((error) => {
+        console.log(error);
+        
+    })
+
+
+
 
 const app = express();
 
