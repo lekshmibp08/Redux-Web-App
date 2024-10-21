8935f936599115adb1804821e84f650cd3aa6778
Lekshmi <sreelekshmi310192@gmail.com>
2024-10-21 22:25:41 +0530

    add redux toolkit

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,15 +1,17 @@
+import axios from 'axios';
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios';
+import { useDispatch, useSelector  } from 'react-redux'
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
 
 
 const SignIn = () => {
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value})
@@ -20,16 +22,13 @@ const SignIn = () => {
     e.preventDefault();
     console.log("Form Data:", formData);
     try {
-      setError(false);  
-      setLoading(true);
+      dispatch(signInStart())
       const res = await axios.post('/api/auth/signin', formData);
-      setLoading(false);
-      setError(false); 
+      dispatch(signInSuccess(res.data)) 
       navigate('/');
           
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error))
       
     }
 
@@ -38,6 +37,7 @@ const SignIn = () => {
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <p className='text-red-700 mt-5 mb-5 text-center'>{error && 'Invalid Credential!' }</p>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         
         <input type="email" placeholder='Email' id='email' 
@@ -57,7 +57,7 @@ const SignIn = () => {
           <span className='text-blue-500'>Sign Up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error && 'Something went wrong!' }</p>
+      
     </div>
   )
 }
