61b1832dc1fb09f4d85d99bf9f58d9de23e1a6c4
Lekshmi <sreelekshmi310192@gmail.com>
2024-10-23 15:14:20 +0530

    completed front end image functionality

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,35 +1,20 @@
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
-<<<<<<< HEAD
 import {app} from '../firebase'
-=======
-import { app } from '../firebase';
->>>>>>> c20df7a (completed image upload functinality)
 
 const Profile = () => {
   const {currentUser} = useSelector((state) => state.user);
   const [image, setImage] = useState(undefined);
-<<<<<<< HEAD
   const [imagePercentage, setImagePercentage] = useState(0)
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
   const fileRef = useRef(null);    
 
-=======
-  const [imagePercentage, setImagePercentage] = useState(0);
-  const [imageError, setImageError] = useState(false);
-  const [formData, setFormData] = useState({});
-  const fileRef = useRef(null)
-  
-  console.log(formData);
-  
->>>>>>> c20df7a (completed image upload functinality)
   useEffect(() => {
     if(image) {
       handleFileUpload(image);
     }
-<<<<<<< HEAD
   },[image]);
 
   const handleFileUpload = (image) => {
@@ -38,38 +23,12 @@ const Profile = () => {
     const fileExtension = image.name.split('.').pop(); // Get file extension
     const fileName = `image_${currentDateTime}.${fileExtension}`; // Create unique file name
     const storageRef = ref(storage, fileName)
-=======
-  }, [image]);
-
-  const handleFileUpload = () => {
-    const storage = getStorage(app);
-    const currentDateTime = new Date().toISOString().replace(/[:.]/g, '-'); 
-    const fileExtension = image.name.split('.').pop(); 
-    const fileName = `image_${currentDateTime}.${fileExtension}`;
-    const storageRef = ref(storage, fileName);
->>>>>>> c20df7a (completed image upload functinality)
     const uploadTask = uploadBytesResumable(storageRef, image);
     uploadTask.on(
       'state_changed',
       (snapshot) => {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImagePercentage(Math.round(progress));
-<<<<<<< HEAD
-        
-      },
-      (error) => {
-        setImageError(true);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref)
-        .then((downloadUrl) => {
-          setFormData({...formData, profilePicture: downloadUrl});
-        })
-      }
-    );
-  }
-  
-=======
       },
       (err) => {
         setImageError(true)
@@ -84,24 +43,15 @@ const Profile = () => {
 
   }
 
->>>>>>> c20df7a (completed image upload functinality)
   return (
     <div className="p-3 max-w-lg mx-auto my-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className="flex flex-col gap-4">
-<<<<<<< HEAD
         <input 
           onChange={(e) => setImage(e.target.files[0])}
           type="file" ref={fileRef} hidden accept="image/*" />
         <img 
           onClick={() => fileRef.current.click()} 
-=======
-        <input type="file" ref={fileRef} hidden accept="image/*" 
-          onChange={(e) => setImage(e.target.files[0])}
-        />
-        <img 
-          onClick={() => fileRef.current.click()}
->>>>>>> c20df7a (completed image upload functinality)
           src={formData.profilePicture || currentUser?.profilePicture}  
           alt="profile" 
           className="h-24 w-24 self-center cursor-pointer 
@@ -111,7 +61,6 @@ const Profile = () => {
         <p className="text-sm self-center">
           {imageError ? (
             <span className="text-red-700">
-<<<<<<< HEAD
               Error Uploading Image (file size must be less than 2 MB)
             </span>
           ) : imagePercentage > 0 && imagePercentage < 100 ? (
@@ -123,17 +72,6 @@ const Profile = () => {
               Image Uploaded Successfully
             </span>
           ) : ( "" )}
-=======
-              Error uploading image(choose image file of size less tha 2MB)
-            </span>
-          ) : imagePercentage > 0 && imagePercentage < 100 ? (
-            <span className="text-slate-700">
-              {`Uploading:  ${imagePercentage} % done`}
-            </span>
-          ) : imagePercentage === 100 ? (
-            <span className="text-green-600">Image uploaded successfully</span>
-          ) : '' }
->>>>>>> c20df7a (completed image upload functinality)
         </p>
 
         <input type="text" id="username" placeholder="Username"
