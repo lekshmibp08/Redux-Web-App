73650ffb88d7394b6f63be6d7dd52789201d13ac
Lekshmi <sreelekshmi310192@gmail.com>
2024-10-24 21:39:30 +0530

    added admin token verification logic

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -14,3 +14,17 @@ export const verifyToken = (req, res, next) => {
     })
 
 }
+
+export const verifyAdmin = (req, res, next) => {
+    const token = req.cookies.admin_token;
+    if(!token) {
+        return next(errorHandler(401, 'Authentication failed!'))
+    }
+    jwt.verify(token, process.env.JWT_SECRET,
+    (err, user) => {
+        if(err) return next(errorHandler(403,'Invalid Token'));
+        req.user = user;
+        next();
+    })
+
+}
