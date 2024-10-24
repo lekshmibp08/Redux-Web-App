af2f65f341ed0e1b383662f9786343fcab8a4526
Lekshmi <sreelekshmi310192@gmail.com>
2024-10-24 14:20:14 +0530

    completed home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const {currentUser} = useSelector((state) => state.user);
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Welcome {currentUser.username}
+      </h1>
+      <p className='mb-4 text-slate-700'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+      <p className='mb-4 text-slate-700'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p className='mb-4 text-slate-700'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+    </div>
   )
 }
 
